d20cdbe91d96eb5850afa9b59f4db5f9fcd2e869
Marek Kadlčík <makadlcik@gmail.com>
2017-02-05 15:11:26 +0100

    print Bing photo of the day url

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ from urllib.request import urlopen
 from urllib.error import URLError
 import json
 
+
+BING_TODAY_JSON_URL = "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US"
+BING_BASE_DOMAIN = "http://www.bing.com"
+
+
 try:
-	response = urlopen("http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US")
-	data = response.read().decode("utf-8")
-	print(json.loads(data))
+	bing_today_json = urlopen(BING_TODAY_JSON_URL).read().decode("utf-8")
+	wallpaper_relative_url = json.loads(bing_today_json)['images'][0]['url']
+	wallpaper_url = BING_BASE_DOMAIN + wallpaper_relative_url
+	print(wallpaper_url)
 except URLError:
 	print("No internet connection!")
