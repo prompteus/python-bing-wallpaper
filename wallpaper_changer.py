2e0310bb8c961a47f3a783dc2e5494d2c33468fe
Marek Kadlčík <makadlcik@gmail.com>
2017-02-05 21:38:22 +0100

    remove support for Gnome 2 and add it for Pantheon

--- wallpaper_changer.py
+++ wallpaper_changer.py
@@ -12,18 +12,23 @@ elif system() == "Darwin":
 
 elif system() == "Linux":
 	import subprocess
+	import os
 	from os import system as s
+
 	def get_output(command):
 		p = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
 		out, err = p.communicate()
 		return out 
+
+	GSETTINGS_COMMAND = 'gsettings set org.gnome.desktop.background picture-uri "file://{0}"'
+
 	try:
-		v = int(get_output("gnome-session --version").split()[-1][0])
-		if v == 2:
+		if int(get_output("gnome-session --version").split()[-1][0]) == 3:
 			def change_wallpaper(uri):
-				s("gconftool-2 --type=string --set /desktop/gnome/background/picture_filename {0}".format(uri))
-		elif v == 3:
-			def change_wallpaper(uri):
-				s('gsettings set org.gnome.desktop.background picture-uri "file://{0}"'.format(uri))
+				s(GSETTINGS_COMMAND.format(uri))
 	except:
-		raise OSError("Wallaper Change is supported in GNOME 2/3 and Unity only")
+		if os.environ.get('DESKTOP_SESSION') == 'pantheon':
+			def change_wallpaper(uri):
+				s(GSETTINGS_COMMAND.format(uri))
+		else:
+			raise OSError("Wallaper change is only supported in GNOME 3, Unity and Pantheon")
