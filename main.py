9fdb8d80f77890a80aaca15156029ad5d4e3f52e
Marek Kadlčík <makadlcik@gmail.com>
2017-02-05 14:27:26 +0100

    download and print Bing json

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import change_wallpaper
+from urllib.request import urlopen
+from urllib.error import URLError
+import json
+
+try:
+	response = urlopen("http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US")
+	data = response.read().decode("utf-8")
+	print(json.loads(data))
+except URLError:
+	print("No internet connection!")
